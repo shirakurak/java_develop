a7e72fceb40277a708a5993e7e431ab32c5f4b76
shirakurak <63482939+shirakurak@users.noreply.github.com>
2020-06-11 20:36:23 +0900

    SuperCSVのサンプルのためのひな型。

---
+++ Ch14/csvSample/Employee.java
@@ -0,0 +1,12 @@
+package Ch14.csvSample;
+
+public class Employee {
+    private String name;
+    private Integer age;
+    private Date birth;
+    private String email;
+    private String note;
+
+    // getter/setterは省略。
+    
+}
